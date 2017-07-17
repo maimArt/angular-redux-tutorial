ca4e2bc600df5849134f9174be2fe2084fc03e69
Martin Maier <martinmaier@gmx.com>
2017-07-17 10:01:30 +0200

    * removed unused selectors

--- src/app/partyplaner/model/redux/selectors.ts
+++
@@ -1,9 +0,0 @@
-import {Person} from "../../../model/person.type";
-
-export function getCountOfPartymembers(state): number {
-  return state.partyplaner.party.members.length;
-}
-
-export function getPartymembers(state): Array<Person> {
-  return state.partyplaner.party.members;
-}
