467e63c14081351294a9ebdebdc9a18b0e41f470
Martin Maier <martinmaier@gmx.com>
2017-07-20 08:19:35 +0200

    * changed to private
    (cherry picked from commit 9615342)

--- src/app/store/planning/selectors.ts
+++ src/app/store/planning/selectors.ts
@@ -2,7 +2,7 @@ import {RootState} from '../root.reducer'
 import {Party} from '../../model/party.type'
 import {PartyplanerState} from './reducer'
 
-export function getPartyplanerState(rootState: RootState): PartyplanerState {
+function getPartyplanerState(rootState: RootState): PartyplanerState {
   return rootState.partyplaner;
 }
 
