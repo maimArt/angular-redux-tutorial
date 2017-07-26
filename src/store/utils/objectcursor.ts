f88f73d6c97f6857573baa5a0abb16a8b2511044
Martin Maier <martinmaier@gmx.com>
2017-07-26 14:20:47 +0200

    * improved cursor and added documentation

--- src/store/utils/objectcursor.ts
+++ src/store/utils/objectcursor.ts
@@ -2,17 +2,28 @@ import * as _ from 'lodash'
 
 export namespace Cursor {
 
+  /**
+   * Class that wraps an object and allows to travers its children recursively
+   **/
   export class ObjectCursor<RT> {
     private rootObject: RT
     private path: string = ''
 
+    /**
+     * default constructor
+     * @param {RT} root traversing object
+     */
     constructor(root: RT) {
       this.rootObject = root
     }
 
-    public to<K extends keyof RT>(key: K): ObjectCursorNode<RT, RT[K]> {
-      let node = this.rootObject[key]
-      this.appendToPath(key)
+    /** move to child node
+     * @param {K} name of the child node
+     * @returns {ObjectCursorNode<RT, RT[K]>} traversable child node
+     **/
+    public to<K extends keyof RT>(childNode: K): ObjectCursorNode<RT, RT[K]> {
+      let node = this.rootObject[childNode]
+      this.appendToPath(childNode)
       return new ObjectCursorNode(this, node)
     }
 
@@ -24,6 +35,9 @@ export namespace Cursor {
       return this.path
     }
 
+    /**
+     * @returns {RT} wrapped object
+     */
     public getObject(): RT {
       return this.rootObject
     }
@@ -40,18 +54,31 @@ export namespace Cursor {
       this.node = node
     }
 
+    /** move to child node
+     * @param {K} name of the child node
+     * @returns {ObjectCursorNode<RT, N[K]>} traversable child node
+     **/
     to<K extends keyof N>(key: K): ObjectCursorNode<RT, N[K]> {
       var branch = this.node[key]
       this.root.appendToPath(key)
       return new ObjectCursorNode(this.root, branch)
     }
 
-    /* type safe setter */
+    /**
+     * sets the value of the node
+     * @param {N} value
+     * @returns {Cursor.ObjectCursorNode<RT, N>} this
+     */
     set(value:N): ObjectCursorNode<RT, N> {
       _.set(this.root.getObject(), this.root.getPath(), value)
       return this;
     }
 
+    /**
+     * sets the value of the node
+     * @param {function:(N) => N} injected function that creates the new value by knowing the old value (e.g. (oldValueArray) => [...oldValueArray, appendedValue]
+     * @returns {Cursor.ObjectCursorNode<RT, N>} this
+     */
     setByFunction(setFunction:(N) => N): ObjectCursorNode<RT, N> {
       let currentvalue = _.get(this.root.getObject(), this.root.getPath());
       let value = setFunction(currentvalue);
@@ -64,7 +91,13 @@ export namespace Cursor {
     }
   }
 
+  /** Same like {@link ObjectCursor} but works on a clone of the object */
   export class ClonedObjectCursor<T> extends ObjectCursor<T> {
+
+    /**
+     * default constructor that clones the object
+     * @param {RT} root traversing object
+     */
     constructor(root: T) {
       super(_.cloneDeep(root))
     }
